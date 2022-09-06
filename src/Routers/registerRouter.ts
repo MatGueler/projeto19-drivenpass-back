f4e2e814c8175e6fd7bfd95ac8b3c3cc19182234
MattGueler <mateus.gueler@gmail.com>
2022-09-06 13:58:50 -0300

    Create valid schema to register user

--- src/Routers/registerRouter.ts
+++ src/Routers/registerRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../Controllers/userController";
+import { validateSchema } from "../Middlewares/validateSchemaMiddleware";
+import registerSchema from "../Schemas/registerSchema";
 
 const RegisterRouter = Router();
 
-RegisterRouter.post("/signup", registerUser);
+RegisterRouter.post("/signup", validateSchema(registerSchema), registerUser);
 RegisterRouter.post("/login", loginUser);
 
 export default RegisterRouter;
